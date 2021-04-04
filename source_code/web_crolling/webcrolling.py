bcd32abdf84b31bb6f4eb32f133cd4655c24907d
장우성 <saywoo1021@gmail.com>
2021-04-04 10:54:52 +0000

    2021/04/04

--- source_code/web_crolling/webcrolling.py
+++ source_code/web_crolling/webcrolling.py
@@ -3,5 +3,6 @@ from bs4 import BeautifulSoup
 
 url = "https://saywoo.github.io/saywoo/"
 ret = requests.get(url)
-
-print(ret.text)
+soup = BeautifulSoup(ret.text, 'html.parser')
+h2 = soup.find('h2')
+print(h2.get_text())
